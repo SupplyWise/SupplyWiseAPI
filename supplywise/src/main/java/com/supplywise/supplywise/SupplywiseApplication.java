92ec60ac3034dc25a8f9c40c43db1746b6658a0b
miragaia <miguelmiragaia2003@gmail.com>
2024-10-07 11:36:48 +0100

    initializing spring project

---
+++ supplywise/src/main/java/com/supplywise/supplywise/SupplywiseApplication.java
@@ -0,0 +1,13 @@
+package com.supplywise.supplywise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplywiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplywiseApplication.class, args);
+	}
+
+}
---
+++ supplywise/src/test/java/com/supplywise/supplywise/SupplywiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supplywise.supplywise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplywiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
