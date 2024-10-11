fce6ac17edd0f289e01fafa816e97c1dc8865a19
Maria Joao <mariasardinha@ua.pt>
2024-10-11 12:50:51 +0100

    fix: validation of username and password

--- supplywise/src/main/java/com/supplywise/supplywise/model/User.java
+++ supplywise/src/main/java/com/supplywise/supplywise/model/User.java
@@ -1,6 +1,8 @@
 package com.supplywise.supplywise.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import lombok.Getter;   // to automatically generate getter methods for all fields
 import lombok.Setter;   // to automatically generate setter methods for all fields
@@ -23,9 +25,13 @@ public class User{
     @Column(name = "id", updatable = false, nullable = false, columnDefinition = "UUID")
     private UUID id;
 
+    @NotBlank(message = "Username is mandatory")
+    @Size(min = 5, max = 255, message = "Username should be between 5 and 255 characters long")
     @Column(name = "username", nullable = false, unique = true, length = 255)
     private String username;
 
+    @NotBlank(message = "Password is mandatory")
+    @Size(max = 255)
     @Column(name = "password", nullable = false, length = 255)
     private String password;
 
