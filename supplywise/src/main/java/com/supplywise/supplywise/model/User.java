d90416d42c6be17315ee9a0c2d0800f79bacbb6b
Maria Joao <mariasardinha@ua.pt>
2024-10-11 10:20:12 +0100

    feat: created User and Roles model entities

---
+++ supplywise/src/main/java/com/supplywise/supplywise/model/User.java
@@ -0,0 +1,55 @@
+package com.supplywise.supplywise.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.*;
+
+import lombok.Getter;   // to automatically generate getter methods for all fields
+import lombok.Setter;   // to automatically generate setter methods for all fields
+import lombok.NoArgsConstructor; // to automatically generate no-args constructor
+import lombok.AllArgsConstructor;   // to automatically generate all args constructor
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User{
+
+    @Id
+    @GeneratedValue()
+    @Column(name = "id", updatable = false, nullable = false, columnDefinition = "UUID")
+    private UUID id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 255)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role = Role.MANAGER;   // default role is Manager
+
+    @ManyToOne(fetch = FetchType.LAZY)  // to the Restaurant entity not be fetched from the database until it is accessed
+    @JoinColumn(name = "restaurant_id", nullable = true)
+    private Restaurant restaurant;
+
+    @Column(name = "created_at", nullable = false)
+    @org.hibernate.annotations.CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @org.hibernate.annotations.UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    
+    public User(String username, String password, Role role, Restaurant restaurant){
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.restaurant = restaurant;
+    }
+}
