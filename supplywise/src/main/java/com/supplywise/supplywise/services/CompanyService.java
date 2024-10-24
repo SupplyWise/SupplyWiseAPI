7f2f6dea65a88a4368358974f7cfcb73a61ae410
miragaia <miguelmiragaia2003@gmail.com>
2024-10-24 19:26:49 +0100

    feat: companyExists function

--- supplywise/src/main/java/com/supplywise/supplywise/services/CompanyService.java
+++ supplywise/src/main/java/com/supplywise/supplywise/services/CompanyService.java
@@ -5,6 +5,8 @@ import com.supplywise.supplywise.repositories.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class CompanyService {
 
@@ -13,5 +15,9 @@ public class CompanyService {
 
     public Company createCompany(Company company) {
         return companyRepository.save(company);
+    }
+
+    public boolean companyExists(UUID id) {
+        return companyRepository.existsById(id);
     }
 }
