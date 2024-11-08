0baedbe2149118f357983bd7aeb0e9140e383e88
Miguel Silva <98617294+miguel-silva48@users.noreply.github.com>
2024-11-08 17:13:40 +0000

    feat: Enhance Item model validation and add UNKNOWN category

--- supplywise/src/main/java/com/supplywise/supplywise/model/Item.java
+++ supplywise/src/main/java/com/supplywise/supplywise/model/Item.java
@@ -3,11 +3,11 @@ package com.supplywise.supplywise.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -20,18 +20,20 @@ public class Item {
     @GeneratedValue
     @Column(name = "id", updatable = false, nullable = false, columnDefinition = "UUID")
     private UUID id;
-
     
+    @NotBlank(message = "Item must have a name")
     @Column(name = "name", nullable = false, length = 255)
+    @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
     private String name;
 
     @Column(name = "barcode", unique = true)
+    @Pattern(regexp = "\\d{3,30}", message = "Bar code must be a numeric string with length between 3 and 30")
     private String barCode;
 
+    @NotBlank(message = "Item must have a category")
     @Enumerated(EnumType.STRING)
     private Category category;
 
-    
     public Item(String name, String barCode, Category category) {
         this.name = name;
         this.barCode = barCode;
--- supplywise/src/main/java/com/supplywise/supplywise/services/ItemService.java
+++ supplywise/src/main/java/com/supplywise/supplywise/services/ItemService.java
@@ -1,14 +1,20 @@
 package com.supplywise.supplywise.services;
 
+import com.supplywise.supplywise.model.Category;
 import com.supplywise.supplywise.model.Item;
 import com.supplywise.supplywise.repositories.ItemRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.EnumSet;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class ItemService {
+
+    private static final int MIN_ITEM_NAME_LENGTH = 3;
+    private static final int MAX_ITEM_NAME_LENGTH = 100;
+    private static final String ITEM_BAR_CODE_REGEX = "\\d{3,30}"; //List of integers with lengths 3 to 30 digits
 
     private final ItemRepository itemRepository;
 
@@ -28,16 +34,41 @@ public class ItemService {
         return itemRepository.findById(id).orElse(null);
     }
 
+    public Item getItemByBarCode(int barCode) {
+        return itemRepository.findByBarCode(barCode).orElse(null);
+    }
+
     public Item updateItem(UUID id, Item itemDetails) {
-        return itemRepository.findById(id).map(item -> {
+        Item item = itemRepository.findById(id).orElse(null);
+        if (item != null) {
             item.setName(itemDetails.getName());
             item.setBarCode(itemDetails.getBarCode());
             item.setCategory(itemDetails.getCategory());
             return itemRepository.save(item);
-        }).orElse(null);
+        }
+        return null;
     }
 
     public void deleteItem(UUID id) {
         itemRepository.deleteById(id);
+    }
+
+    /* Helper functions */
+
+    public boolean isItemValid(Item item) {
+        String itemName = item.getName();
+        String itemBarCode = item.getBarCode();
+        Category itemCategory = item.getCategory();
+
+        if (itemName == null || itemName.isEmpty() || itemName.length() < MIN_ITEM_NAME_LENGTH || itemName.length() > MAX_ITEM_NAME_LENGTH) {
+            return false;
+        }
+        if (itemBarCode == null || !itemBarCode.matches(ITEM_BAR_CODE_REGEX)) {
+            return false;
+        }
+        if (itemCategory == null || !EnumSet.allOf(Category.class).contains(itemCategory)) {
+            return false;
+        }
+        return true;
     }
 }
